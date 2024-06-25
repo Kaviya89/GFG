7a2a12c35258c1735102545302693f66f4ccd151
Kaviya89 <71933883+Kaviya89@users.noreply.github.com>
2024-06-25 19:19:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a, n);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        // code here 
+        for (int i = 0; i < n; i++) {
+            while (arr[i] != arr[arr[i] - 1]) {
+                // Swap arr[i] with the element at its correct position
+                int correctIndex = arr[i] - 1;
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }
+        }
+        int[] k = new int[2];
+        for(int j=0; j<n;j++){
+            if(arr[j]!=j+1){
+                k[0]=arr[j];
+                k[1]=j+1;
+                break;
+            }
+        }
+        return k;
+    }
+}
